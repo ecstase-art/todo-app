7e6209bdc37fa4a93a336cd9db8401804fdfafd5
EcStasE <ecstase@gmail.com>
2026-08-11 10:35:35 +1000

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    deadline TIMESTAMP,
+    notify_minutes INTEGER DEFAULT 0
 );
